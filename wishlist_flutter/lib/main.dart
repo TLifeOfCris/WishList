5fe2610b6d6d9a7d1ef18ddc5c2448a0aa88fb83
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-19 11:45:58 -0600

    Se agregó Provider al Main

--- wishlist_flutter/lib/main.dart
+++ wishlist_flutter/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wishlist_flutter/Screens/HomeScreen.dart';
+import 'package:wishlist_flutter/provider/viewmodesls/providerviewmodel.dart';
 
 void main() {
-  runApp(const MyApp());
+  
+  runApp(
+    
+    
+    ChangeNotifierProvider(create: (_) => WishlistProvider(),
+    child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
