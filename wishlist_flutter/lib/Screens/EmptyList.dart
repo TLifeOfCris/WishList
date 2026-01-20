902c204a84ba3604825534f3dd076d95032a7b65
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-19 23:00:49 -0600

    Se agregó ruta a boton AddWish

--- wishlist_flutter/lib/Screens/EmptyList.dart
+++ wishlist_flutter/lib/Screens/EmptyList.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wishlist_flutter/Screens/AddWishScreen.dart';
 import 'package:wishlist_flutter/widgets/cuppertinoButton.dart';
 
 class EmptyList extends StatelessWidget {
@@ -18,7 +20,9 @@ class EmptyList extends StatelessWidget {
               letterSpacing: 0.2
             ),),
             SizedBox(height: 16,),
-            IOSButton(text: 'Add Wish', onPressed: (){})
+            IOSButton(text: 'Add Wish', onPressed: (){
+              Navigator.of(context).push(CupertinoPageRoute(builder: (_) => AddWishScreen()));
+            })
           ],
         ),) ;
   }
