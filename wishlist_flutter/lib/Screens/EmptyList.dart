6fcda802aad3aa8da494524b4e6046df68b3d619
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-19 18:29:17 -0600

    Se agregó una nueva screen para la Lista vacía y así limpiar un poco el homeScreen

---
+++ wishlist_flutter/lib/Screens/EmptyList.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:wishlist_flutter/widgets/cuppertinoButton.dart';
+
+class EmptyList extends StatelessWidget {
+  const EmptyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center( child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+        
+            Text('No wishes',style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey.shade600,
+              letterSpacing: 0.2
+            ),),
+            SizedBox(height: 16,),
+            IOSButton(text: 'Add Wish', onPressed: (){})
+          ],
+        ),) ;
+  }
+}
--- wishlist_flutter/lib/Screens/HomeScreen.dart
+++ wishlist_flutter/lib/Screens/HomeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wishlist_flutter/Screens/EmptyList.dart';
 import 'package:wishlist_flutter/models/wishmodel.dart';
 import 'package:wishlist_flutter/provider/viewmodesls/providerviewmodel.dart';
 import 'package:wishlist_flutter/widgets/cuppertinoButton.dart';
@@ -17,21 +18,7 @@ class HomeScreen extends StatelessWidget {
         middle: Text('WishList'),
       ),
       child: SafeArea(
-        child: wishes.isEmpty ?  Center( child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-        
-            Text('No wishes',style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey.shade600,
-              letterSpacing: 0.2
-            ),),
-            SizedBox(height: 16,),
-            IOSButton(text: 'Add Wish', onPressed: (){})
-          ],
-        ),) : ListView.builder(
+        child: wishes.isEmpty ?  EmptyList(): ListView.builder(
           itemCount: wishes.length,
           itemBuilder: (context, index){
             return CupertinoListTile(
